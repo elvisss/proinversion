90bb3240375b0acfc4c2f38461367226816e9391
Elvis-Saavedra <elvis.saavedra.segura@gmail.com>
2016-11-21 18:43:56 -0500

    added yt js file

---
+++ app/js/yt.js
@@ -0,0 +1,25 @@
+$(function() {
+
+  $('.modal').on('hide.bs.modal', function () {
+    var data_video = $(this).data('video');
+    yt_players[data_video].pauseVideo();
+  });
+
+});
+
+var yt_int, yt_players = {},
+
+initYT = function() {
+  $(".ytplayer").each(function() {
+    yt_players[this.id] = new YT.Player(this.id);
+  });
+};
+
+$.getScript("//www.youtube.com/player_api", function() {
+  yt_int = setInterval(function(){
+    if(typeof YT === "object"){
+      initYT();
+      clearInterval(yt_int);
+    }
+  },500);
+});
